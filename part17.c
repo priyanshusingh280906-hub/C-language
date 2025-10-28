8d4b27357d9cfabd422cdd22546620acd585db82
Priyanshu Singh <priyanshusingh280906@gmail.com>
2025-10-28 22:51:39 +0530

    first change

--- part17.c
+++ part17.c
@@ -2,20 +2,23 @@
 
 int main()
 {
-    int a;
+    int a, b = 8;//humlog b ko bas condition check karne ke liya hai
     printf("Enter the number:\n");
     scanf("%d", &a);
     for (int i = 2; i < a; i++)
     {
         if (a % i == 0)
         {
-            printf("%d is a prime number\n", a);
-            break;
+            b = 2;// like condition true hua toh b ka valur 2 ho jayega
         }
-        else
-        {
-            printf("%d is not a prime number\n", a);
-        }
+    }
+    if (b==2)// aur == means equals to 
+    {
+        printf("%d is not a prime number\n", a);
+    }
+    else// agar b==2 nahi hua to to condition false hoga uar b==8 ho jayega aur else statment print ho jayega
+    {
+        printf("%d is a prime number\n", a);
     }
 
     return 0;
